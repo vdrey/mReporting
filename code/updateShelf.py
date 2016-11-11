022c2718607639b1641fc789e9cd8a7bb1cce7e3
Daniel Reynolds <daniel7reynolds@gmail.com>
2016-11-10 23:33:50 -0500

    update updateShelf.py

--- code/updateShelf.py
+++ code/updateShelf.py
@@ -24,10 +24,31 @@ cells['equity_percent'] = {'worksheet':'All','cell':'E4'}
 cells['fixed_percent'] = {'worksheet':'All','cell':'E17'}
 cells['cash_percent'] = {'worksheet':'All','cell':'E25'}
 
+# Asset Values
+cells['LGMAX_val'] = {'worksheet':'All','cell':'E5'}
+cells['LGRRX_val'] = {'worksheet':'All','cell':'E6'}
+cells['V00_val'] = {'worksheet':'All','cell':'E7'}
+cells['VB_val'] = {'worksheet':'All','cell':'E8'}
+cells['VWO_val'] = {'worksheet':'All','cell':'E9'}
+cells['VNQ_val'] = {'worksheet':'All','cell':'E10'}
+cells['IRDM_val'] = {'worksheet':'All','cell':'E11'}
+cells['MSTX_val'] = {'worksheet':'All','cell':'E12'}
+cells['SHY_val'] = {'worksheet':'All','cell':'E18'}
+cells['LQD_val'] = {'worksheet':'All','cell':'E19'}
+cells['DDF_val'] = {'worksheet':'All','cell':'E20'}
 
-
-
-
+# Number of shares
+cells['LGMAX_shares'] = {'worksheet':'All','cell':'C5'}
+cells['LGRRX_shares'] = {'worksheet':'All','cell':'C6'}
+cells['V00_shares'] = {'worksheet':'All','cell':'C7'}
+cells['VB_shares'] = {'worksheet':'All','cell':'C8'}
+cells['VWO_shares'] = {'worksheet':'All','cell':'C9'}
+cells['VNQ_shares'] = {'worksheet':'All','cell':'C10'}
+cells['IRDM_shares'] = {'worksheet':'All','cell':'C11'}
+cells['MSTX_shares'] = {'worksheet':'All','cell':'C12'}
+cells['SHY_shares'] = {'worksheet':'All','cell':'C18'}
+cells['LQD_shares'] = {'worksheet':'All','cell':'C19'}
+cells['DDF_shares'] = {'worksheet':'All','cell':'C20'}
 
 
 
