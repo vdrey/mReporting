23c17e0b17cfbae28c900f4f640c725108e93ee4
Daniel Reynolds <daniel7reynolds@gmail.com>
2016-11-26 15:48:29 -0500

    update morning2

--- code/morning2.py
+++ code/morning2.py
@@ -5,7 +5,8 @@ import cell
 # Create a list of values to update.
 vals = ['total_val','equity_val','fixed_val','cash_val','wfa_val','acorns_val','robinhood_val','LGMAX_val','LGRRX_val',
         'VOO_val','VB_val','VWO_val','VNQ_val','IRDM_val','MSTX_val','SHY_val','LQD_val','DDF_val','LGMAX_shares','LGRRX_shares',
-        'VOO_shares','VB_shares','VWO_shares','VNQ_shares','IRDM_shares','MSTX_shares','SHY_shares','LQD_shares','DDF_shares']
+        'VOO_shares','VB_shares','VWO_shares','VNQ_shares','IRDM_shares','MSTX_shares','SHY_shares','LQD_shares','DDF_shares',
+        'CHKR_val','CHKR_shares']
 
 # Open the shelf
 cells = shelve.open('cell_store')
