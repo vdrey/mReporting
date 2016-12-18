b47a500dfcc302f1a8119572fec9727caa77d59a
Daniel Reynolds <daniel7reynolds@gmail.com>
2016-12-17 21:06:50 -0500

    add week close

---
+++ code/weekClose1.py
@@ -0,0 +1,16 @@
+# This script updates all of the weekly opening values
+import shelve
+import cell
+
+# List of cells to update
+# Create a list of values to update.
+vals = ['total_val','equity_val','fixed_val','cash_val','wfa_val','acorns_val','robinhood_val','LGMAX_val','LGRRX_val',
+        'VOO_val','VB_val','VWO_val','VNQ_val','IRDM_val','MSTX_val','SHY_val','LQD_val','DDF_val','CHKR_val']
+
+# Open the shelf
+cells = shelve.open('cell_store')
+
+# go value-by-value and update the morning value.
+for item in vals:
+    wk_cls_Item = 'wk_cls_' + item
+    cell.setVal('Other', cells[wk_cls_Item]['worksheet'], cells[wk_cls_Item]['cell'], cell.getVal('Other', cells[item]['worksheet'],cells[item]['cell']))   
